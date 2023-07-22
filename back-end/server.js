34dba6959a570cf707618baafe2e95f1c7f2b5ec
AndrewKamadulski <andrew.kamadulski@gmail.com>
2023-07-22 12:23:54 -0500

    fixing signup message

--- back-end/server.js
+++ back-end/server.js
@@ -4,64 +4,46 @@ const express = require('express');
 const cors= require('cors');
 const nodemailer = require('nodemailer')
 const dotenv = require('dotenv').config()
-
-
-
+const sgMail = require("@sendgrid/mail");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-
-app.get('/photos/main/:id', (req, res) => {
-  let id = req.params.id
-  res.sendFile(path.join(__dirname, `./images/main/${id}.jpg`))
+app.get("/photos/main/:id", (req, res) => {
+  let id = req.params.id;
+  res.sendFile(path.join(__dirname, `./images/main/${id}.jpg`));
 });
 
-app.get('/photos/about/:id', (req, res) => {
-  let id = req.params.id
-  res.sendFile(path.join(__dirname, `./images/about/${id}.jpg`))
+app.get("/photos/about/:id", (req, res) => {
+  let id = req.params.id;
+  res.sendFile(path.join(__dirname, `./images/about/${id}.jpg`));
 });
 
-app.get('/Gallery/:gallery/:id', (req, res) => {
-  let id = req.params.id
-  let gallery = req.params.gallery
-  res.sendFile(path.join(__dirname, `./images/galleries/${gallery}/${id}.jpg`))
+app.get("/Gallery/:gallery/:id", (req, res) => {
+  let id = req.params.id;
+  let gallery = req.params.gallery;
+  res.sendFile(path.join(__dirname, `./images/galleries/${gallery}/${id}.jpg`));
 });
 
-app.post('/send_mail', (req,res) => { 
-  
-  
+app.post("/send_mail", (req, res) => {
   const { email, option, name, phone, message } = req.body;
-    const transporter = nodemailer.createTransport({
-        service:"goDaddy",
-        host: "smtp.office365.com",
-        port: "587",
-        secure: false,
-        requireTLS: true,
-        auth: {
-           user: process.env.REACT_APP_EMAIL_USER,
-           pass: process.env.REACT_APP_EMAIL_PASS 
-       },
-       tls: {
-           ciphers:'SSLv3',
-           rejectUnauthorized: false 
-       }
-    });
-  
-    let sendMessage = {
-        from: "shellyk@lilithphotography.com",
-        to: "shellyk@lilithphotography.com, lilithphotography34@gmail.com, andrew.kamadulski@gmail.com",
-        subject: "New Message Request from LilithPhotography.com",
-        html: `
+
+  sgMail.setApiKey(process.env.SGMAIL_API_KEY);
+
+  const msg = {
+    to: ["kamadulski999@gmail.com", "Lilithphotography34@gmail.com"],
+    from: "andrew.kamadulski@gmail.com",
+    subject: "New Message Request from LilithPhotography.com",
+    text: "message: ",
+    html: `
         <hr />        
         <span>Name: ${name}</span><br/>
         <span>Email: "${email}"</span><br/>
@@ -70,18 +52,19 @@ app.post('/send_mail', (req,res) => {
         <span>Message:</span><br/>
         <p>${message || "No message"}</p>
     <hr />
-        `
+        `,
+  };
+
+  sgMail.send(msg).then(
+    () => {},
+    (error) => {
+      console.error(error);
+
+      if (error.response) {
+        console.error(error.response.body);
+      }
     }
- 
-    transporter.sendMail(sendMessage, function (err, info) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('sent', info);
-        }
-    });
- 
-    res.sendStatus(200);
+  );
 });
 
 
